6539aed206f31c59bab2e34f251446e673151863
esado95 <taymus01@gmail.com>
2026-07-27 18:06:12 +0200

    Единое написание процентов

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Кэш приложения: работает офлайн, обновляется при смене VERSION
-const VERSION = 'fullbody-v23';
+const VERSION = 'fullbody-v24';
 const ASSETS = ['.', 'index.html', 'data.js?v=7', 'manifest.json', 'icon-180.png', 'icon-512.png'];
 
 self.addEventListener('install', e => {
